9c4ac097551d877bac7d8aad5475859c44ed75a3
Silverhov <belexden@gmail.com>
2024-03-05 01:40:31 +0700

    Апгрейд

---
+++ Child Game/Assets/QuestionsAndAnswers.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Questions : MonoBehaviour
+{
+    public string Question;
+    public string [] Answers;
+    public  int CorrentAnswer;
+    
+}
